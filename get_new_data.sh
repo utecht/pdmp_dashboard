fcbfabafe5c5ef4db21a5224bb388af14e9ac0b7
Joseph Utecht <thenmal@gmail.com>
2020-04-17 16:13:31 -0500

    moved stuff around

--- get_new_data.sh
+++ get_new_data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget https://github.com/nytimes/covid-19-data/raw/master/us-counties.csv
-./update_covid_data.py > ar_pop_demo/covid_numbers.js
+./update_covid_data.py > covid_numbers.js
 rm us-counties.csv
